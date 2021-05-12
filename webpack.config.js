6da1891e43493b8c9e674cafe88b453b131cda9a
Christopher Guyton <christopherguyton87@gmail.com>
2021-05-11 20:10:02 -0700

    updated build command

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
           test: /\.(png|jpe?g|gif)$/i,
           use: [
             {
-              loader: ['file-loader', 'babel-loader', 'webpack-conditional-loader'],
+              loader: 'file-loader',
               options: {
                 name: '[path][name].[ext][query]',
               },
